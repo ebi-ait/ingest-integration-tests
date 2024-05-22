ffa176ce4056724f559052677063bf6aa782e367
Amnon Khen <amnon@ebi.ac.uk>
2024-05-22 13:52:50 +0100

    dcp-967 fast failure for integration test

--- tests/runners/submission_manager.py
+++ tests/runners/submission_manager.py
@@ -79,13 +79,13 @@ class SubmissionManager:
 
     def wait_for_envelope_metadata_to_be_validated(self):
         Progress.report("WAIT FOR VALIDATION...")
-        WaitFor(self._envelope_is_in_state, 'Metadata valid').to_return_value(
-            value=True, fail_if="Metadata invalid")
+        WaitFor(self._get_envelope_state) \
+            .to_return_value(value='Graph valid', fail_if="Metadata invalid")
         Progress.report(" envelope metadata is valid.\n")
 
     def wait_for_envelope_to_have_valid_graph(self):
         Progress.report("WAIT FOR GRAPH VALIDATION...")
-        WaitFor(self._envelope_is_in_state, 'Graph valid').to_return_value(value=True, fail_if="Graph invalid")
+        WaitFor(self._get_envelope_state, ).to_return_value(value='Graph valid', fail_if="Graph invalid")
         Progress.report(" envelope graph is valid.\n")
 
     def wait_for_envelope_to_be_submitted(self):
@@ -134,7 +134,12 @@ class SubmissionManager:
         envelope_status = self.submission_envelope.reload().status()
         Progress.report(f"envelope status is {envelope_status}")
         return envelope_status in [state]
-        
+
+    def _get_envelope_state(self):
+        envelope_status = self.submission_envelope.reload().status()
+        Progress.report(f"envelope status is {envelope_status}")
+        return envelope_status
+
     def ensure_submitted(self):
         try:
             self.submit_envelope()
